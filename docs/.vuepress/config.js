8ef22b2a5eec164358212a71c1c8d8c576403460
sunzhipeng <sunzhipeng@58.com>
2024-06-04 00:08:45 +0800

    feat: 添加搜索插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
+import { searchProPlugin } from 'vuepress-plugin-search-pro';
 import { navbar } from './config/navbar';
 import { sidebar } from './config/sidebar';
 
@@ -8,12 +9,16 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: 'bestvist blog',
   description: 'bestvist blog',
-
   theme: defaultTheme({
     logo: '/images/hero.png',
     navbar,
     sidebar,
   }),
-
+  plugins: [
+    searchProPlugin({
+      indexContent: true,
+      hotReload: true
+    })
+  ],
   bundler: viteBundler(),
 })
